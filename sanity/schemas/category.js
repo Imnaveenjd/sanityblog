0d923ea9fee47aabc3344548ffb4b257389464e6
Imnaveenjd <imnaveenjd@gmail.com>
2022-07-14 11:19:22 +0530

    Finished build

---
+++ sanity/schemas/category.js
@@ -0,0 +1,22 @@
+export default {
+  name: 'category',
+  title: 'Category',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    },
+    {
+      name: 'post',
+      type: 'reference',
+      to: {type: 'author'},
+    },
+  ],
+};
